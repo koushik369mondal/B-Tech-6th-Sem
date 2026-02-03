6c697752783bdaef45e798280928ee81ebe5157b
Kaushik Mandal <koushik369mondal@gmail.com>
2026-02-03 09:31:21 +0530

    Add a new Java class 'raviSir' with a method to add two numbers and print the result

---
+++ DSA-JAVA/raviSir.java
@@ -0,0 +1,11 @@
+public class raviSir {
+    public static void add(){
+        int x = 5;
+        int y = 6;
+        int z = x + y;
+        System.out.println(z);
+    }
+    public static void main(String[] args) {
+        add();
+    }
+}
