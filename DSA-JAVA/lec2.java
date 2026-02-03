79c8ff29a64fba105d438b907354e9d2f16a1c4d
Kaushik Mandal <koushik369mondal@gmail.com>
2026-02-03 10:11:40 +0530

    Add allEven method to print all even numbers up to user input

--- DSA-JAVA/lec2.java
+++ DSA-JAVA/lec2.java
@@ -73,6 +73,17 @@ public class lec2 {
         }
     }
 
+    public static void allEven(){
+        System.out.println("Enter a number: ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=1; i<=n; i++){
+            if(i % 2 == 0){
+                System.out.print(i + " ");
+            }
+        }
+    }
+
     public static void main(String[] args) {
         // add();
         // simpleInterest();
