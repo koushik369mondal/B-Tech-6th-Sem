e14800d4c4e17a7563f12f75c75b3967097c1700
Kaushik Mandal <koushik369mondal@gmail.com>
2026-02-03 21:30:39 +0530

    Add sumAllDigits method and update main to call it

--- DSA-JAVA/HW/lec2.java
+++ DSA-JAVA/HW/lec2.java
@@ -35,10 +35,22 @@ public class lec2 {
         System.out.println("Volume of the hemisphere: " + v);
     }
 
+    public static void sumAllDigits(){
+        int n = 123;
+        int sum = 0;
+        while(n > 0){
+            int digit = n % 10;
+            sum = sum + digit;
+            n = n / 10;
+        }
+        System.out.println("Sum of all digits: " + sum);
+    }
+
     public static void main(String[] args) {
         // sumOdd();
         // coneVolume();
         // cylinderVolume();
-        hemisphereVolume();
+        // hemisphereVolume();
+        sumAllDigits();
     }
 }
