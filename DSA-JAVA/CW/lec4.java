3a4a4e479f5d3b779dd013884b75f8f910bba7b2
Kaushik Mandal <koushik369mondal@gmail.com>
2026-02-05 09:34:45 +0530

    use while loops for star pattern output

--- DSA-JAVA/CW/lec4.java
+++ DSA-JAVA/CW/lec4.java
@@ -1,14 +1,29 @@
 package CW;
+
 public class lec4 {
-    public static void pattern1(){
-        for(int i=1; i<=5; i++){
-            //Star 
-            for(int j=1; j<=5; j++){
+    public static void pattern1() {
+        // for (int i = 1; i <= 5; i++) {
+        //     // Star
+        //     for (int j = 1; j <= 5; j++) {
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();
+        // }
+
+        int n = 5;
+        int row = 1;
+        while (row <= n) {
+            // star
+            int i = 1;
+            while (i <= n) {
                 System.out.print("*  ");
+                i++;
             }
             System.out.println();
+            row++;
         }
     }
+
     public static void main(String[] args) {
         pattern1();
     }
