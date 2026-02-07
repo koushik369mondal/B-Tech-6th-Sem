9f293c1c1e7b77ec56e927f1fe4d42e067eb15f2
Kaushik Mandal <koushik369mondal@gmail.com>
2026-02-07 19:53:49 +0530

    square star pattern using while loop

--- DSA-JAVA/HW/patterns.java
+++ DSA-JAVA/HW/patterns.java
@@ -9,11 +9,26 @@ public class patterns {
     // * * * * *
     public static void pattern1(){
         int n = 5;
+
+        System.out.println("Using For Loop : ");
         for(int i=1; i<=n; i++){
             for(int j=1; j<=n; j++){
                 System.out.print(" * ");
             }
             System.out.println();
+        }
+
+        System.out.println("Using While Loop : ");
+        int row = 1;
+        while(row <= n){
+            //star
+            int i = 1;
+            while(i<=n){
+                System.out.print(" * ");
+                i++;
+            }
+            System.out.println();
+            row++;
         }
     }
     public static void main(String[] args) {
