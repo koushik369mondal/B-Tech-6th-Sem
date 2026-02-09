25dcd160dec48036cbd4b0cbd863a67b0220c00e
Kaushik Mandal <koushik369mondal@gmail.com>
2026-02-09 10:00:12 +0530

    pattern2 method for pyramid star pattern

--- DSA-JAVA/CW/lec6.java
+++ DSA-JAVA/CW/lec6.java
@@ -12,13 +12,13 @@ public class lec6 {
     public static void pattern1() {
         int n = 5;
 
-        // System.out.println("Using For Loop : ");
-        // for(int i=1; i<=n; i++){
-        // for(int j=1; j<=i; j++){
-        // System.out.print(" * ");
-        // }
-        // System.out.println();
-        // }
+        System.out.println("Using For Loop : ");
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
 
         System.out.println("Using While Loop : ");
         int row = 1;
@@ -40,7 +40,50 @@ public class lec6 {
         }
     }
 
+    //         *
+    //       * *
+    //     * * *
+    //   * * * *
+    // * * * * *
+    //   * * * *
+    //     * * *
+    //       * *
+    //         *
+    public static void pattern2() {
+        int n = 5;
+
+        System.out.println("Using For Loop : ");
+
+        System.out.println("Using While Loop : ");
+        int space = n - 1;
+        int star = 1;
+        int row = 1;
+
+        while (row <= 2 * n - 1) {
+            int i = 1;
+            while (i <= space) {
+                System.out.print("   ");
+                i++;
+            }
+            int j = 1;
+            while (j <= star) {
+                System.out.print(" * ");
+                j++;
+            }
+            if (row < n) {
+                star++;
+                space--;
+            } else {
+                star--;
+                space++;
+            }
+            row++;
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         pattern1();
+        pattern2();
     }
 }
