4d2d5453a83c4e0b9359d8e3ff8fb4907f8bb084
Kaushik Mandal <koushik369mondal@gmail.com>
2026-02-12 09:59:45 +0530

    Refactor evenOdd2 method to print results directly and update max1 and max2 methods for consistency; clean up main method calls.

--- DSA-JAVA/CW/lec9.java
+++ DSA-JAVA/CW/lec9.java
@@ -11,24 +11,42 @@ public class lec9 {
     }
 
     public static int evenOdd2(int n){
-        if(n%2==0){
-            return 1;
-        }else{
-            return 0;
-        }
-    }
-
-    public static void main(String[] args) {
-        //Using void function
-        int n = 10;
-        evenOdd1(n);
-
-       //Using int function
-        int result = evenOdd2(n);
-        if(result == 1){
+        if (n % 2 == 0) {
             System.out.println("Even");
         } else {
             System.out.println("Odd");
         }
+        return n;
+    }
+
+    public static void max1(int a, int b){
+        if(a>b){
+            System.out.println(a);
+        }else{
+            System.out.println(b);
+        }
+    }
+
+    public static int max2(int a, int b) {
+        if (a > b) {
+            System.out.println(a);
+        } else {
+            System.out.println(b);
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {      
+        int n = 10;
+
+        // Using void function
+        evenOdd1(n);
+
+        //Using return type function
+        evenOdd2(n);
+
+        int a = 5, b = 10;
+        max1(a, b);
+        max2(a, b);
     }
 }
