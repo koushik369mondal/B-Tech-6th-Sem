37125d44d392e71fb0a8cacf18fb1b2e3902693e
Kaushik Mandal <koushik369mondal@gmail.com>
2026-03-01 19:34:26 +0530

    Leetcode-35. Search Insert Position

--- DSA-JAVA/Leetcode/Easy/Leet_35.java
+++ DSA-JAVA/Leetcode/Easy/Leet_35.java
@@ -1,5 +1,26 @@
 package Leetcode.Easy;
 
 public class Leet_35 {
-    
+    public int searchInsert(int[] nums, int target) {
+        int left = 0; 
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2; // find middle
+            if (nums[mid] == target) {
+                return mid; // target found
+            }
+            if (nums[mid] < target) {
+                left = mid + 1; // search right half
+            } else {
+                right = mid - 1; // search left half
+            }
+        }
+        return left;
+    }
+    public static void main(String[] args) {
+            Leet_35 obj = new Leet_35();
+            int[] nums = {1,3,5,6};
+            int target = 5;
+            System.out.println(obj.searchInsert(nums, target));
+    }
 }
