75a9136743044d5a8fb2f60a2dbc3b952d17e4f2
Kaushik Mandal <koushik369mondal@gmail.com>
2026-03-01 20:46:28 +0530

    Add Leet_66 class to implement plusOne method for incrementing an array of digits

---
+++ DSA-JAVA/Leetcode/Easy/Leet_66.java
@@ -0,0 +1,24 @@
+package Leetcode.Easy;
+
+public class Leet_66 {
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int[] newDigits = new int[digits.length + 1];
+        newDigits[0] = 1;
+        return newDigits;
+    }
+    public static void main(String[] args) {
+            Leet_66 obj = new Leet_66();
+            int[] digits = {1,2,3};
+            int[] result = obj.plusOne(digits);
+            for (int num : result) {
+                System.out.print(num + " ");
+            }
+    }
+}
