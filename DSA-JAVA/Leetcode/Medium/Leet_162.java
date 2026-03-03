515e03ef11287534a253813ea568d17afd625dc0
Kaushik Mandal <koushik369mondal@gmail.com>
2026-03-03 21:33:09 +0530

    Refactor Leet_162 class to implement findPeakElement method using binary search for improved efficiency; add Leet_852 class to implement peakIndexInMountainArray method for finding peak index in a mountain array

--- DSA-JAVA/Leetcode/Medium/Leet_162.java
+++ DSA-JAVA/Leetcode/Medium/Leet_162.java
@@ -4,25 +4,22 @@ public class Leet_162 {
     
     public int findPeakElement(int[] nums) {
         int n = nums.length;
-        int index = 0;
-        int peak = 0;
-        for (int i = 0; i < n; i++) {
-            if( i == 0){
-                peak = nums[i];
-                index = i;
-            } 
-            if (nums[i] > peak) {
-                peak = nums[i];
-                index = i;
+        int left = 0;
+        int right = n - 1;
+        while (left < right) {
+            int mid = (right + left) / 2;
+            if (nums[mid] > nums[mid + 1]) {
+                right = mid;
+            } else {
+                left = mid + 1;
             }
         }
-        return index;
+        return left;
     }
 
     public static void main(String[] args) {
         Leet_162 obj = new Leet_162();
         int[] nums = {1, 2, 3, 1};
-        int result = obj.findPeakElement(nums);
-        System.out.println(result);
+        System.out.println(obj.findPeakElement(nums));
     }
 }
---
+++ DSA-JAVA/Leetcode/Medium/Leet_852.java
@@ -0,0 +1,26 @@
+package Leetcode.Medium;
+
+public class Leet_852 {
+
+    public int peakIndexInMountainArray(int[] arr) {
+        int start = 1;
+        int end = arr.length-2;
+        while(start < end){
+            int mid = start + (end - start) / 2;          
+            if(arr[mid-1] < arr[mid] && arr[mid] > arr[mid+1]){
+                return mid;
+            } else if(arr[mid-1] < arr[mid]){ // increasing
+                start = mid + 1;
+            } else { // decreasing
+                end = mid - 1;
+            }
+        }
+        return start;
+    }
+
+    public static void main(String[] args) {
+        Leet_852 obj = new Leet_852();
+        int[] arr = {0, 1, 0};
+        System.out.println(obj.peakIndexInMountainArray(arr));
+    }
+}
