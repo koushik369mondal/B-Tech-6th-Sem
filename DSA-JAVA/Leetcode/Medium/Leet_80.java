7c7d1ebdd0a7d3ef470ec683ad015aef72f8c2f7
Kaushik Mandal <koushik369mondal@gmail.com>
2026-03-05 00:16:58 +0530

    Add Leet_80 class to implement removeDuplicates method for removing duplicates from a sorted array

---
+++ DSA-JAVA/Leetcode/Medium/Leet_80.java
@@ -0,0 +1,20 @@
+package Leetcode.Medium;
+
+public class Leet_80 {
+    public int removeDuplicates(int[] nums) {
+        int k = 2;
+        for (int i=2; i<nums.length; i++) {
+            if (nums[i] != nums[k-2]) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0,1,1,1,2,2,3,3,3,4};
+        Leet_80 obj = new Leet_80();
+        System.out.println(obj.removeDuplicates(nums));
+    }
+}
