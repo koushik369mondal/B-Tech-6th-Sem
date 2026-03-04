3602f0c6049d36558fd69b036a4ee8968e873cc4
Kaushik Mandal <koushik369mondal@gmail.com>
2026-03-04 23:22:35 +0530

    Implement Leet_26 class to remove duplicates from a sorted array and return the new length

---
+++ DSA-JAVA/Leetcode/Easy/Leet_26.java
@@ -0,0 +1,21 @@
+package Leetcode.Easy;
+
+public class Leet_26 {
+    public int removeDuplicates(int[] nums) {
+        int count = 0;
+        for(int i=1; i<nums.length; i++){
+            if(nums[i] == nums[count]){
+                continue;
+            } else {
+                count++;
+                nums[count] = nums[i];
+            }
+        }
+        return count+1;
+    }
+    public static void main(String[] args) {
+        int[] nums = {0,0,1,1,1,2,2,3,3,4};
+        Leet_26 obj = new Leet_26();
+        System.out.println(obj.removeDuplicates(nums));
+    }
+}
--- DSA-JAVA/Leetcode/Medium/Leet_33.java
+++ DSA-JAVA/Leetcode/Medium/Leet_33.java
@@ -2,17 +2,17 @@ package Leetcode.Medium;
 
 public class Leet_33 {
     public int search(int[] nums, int target) {
-        int start = 0, end = nums.length - 1;
-        while(start <= end) {
+        int start = 0, end = nums.length-1;
+        while(start <= end){
             int mid = start + (end - start) / 2;
-            if(nums[mid] == target) {
+            if(nums[mid] == target){
                 return mid;
             }
-            if(nums[start] <= nums[mid]){ //left sorted
-                if(nums[start] <= target && target < nums[mid]){
-                    end = mid - 1;
+            if(nums[start] <= nums[mid]){ // left sorted
+                if(nums[start] <= target && target <= nums[mid]){
+                    end = mid - 1; 
                 } else {
-                    start = mid + 1;
+                    start = mid + 1; 
                 }
             } else { // right sorted
                 if(nums[mid] <= target && target <= nums[end]){
@@ -24,6 +24,7 @@ public class Leet_33 {
         }
         return -1;
     }
+
     public static void main(String[] args) {
         int[] nums = {4,5,6,7,0,1,2};
         int target = 0;
