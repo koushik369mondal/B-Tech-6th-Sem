d8ca9555df50bda0ac279e8edb67cef6681b3da0
Kaushik Mandal <koushik369mondal@gmail.com>
2026-03-06 10:54:21 +0530

    refactor Leet_1 class to optimize twoSum method and improve readability

--- DSA-JAVA/Leetcode/Easy/Leet_1.java
+++ DSA-JAVA/Leetcode/Easy/Leet_1.java
@@ -2,21 +2,23 @@ package Leetcode.Easy;
 
 public class Leet_1 {
     public int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
                 if (nums[i] + nums[j] == target) {
-                    return new int[]{i, j};
+                    int result[] = { i, j };
+                    return result;
                 }
             }
         }
-        return new int[]{-1, -1}; 
+        return new int[] {};
     }
 
     public static void main(String[] args) {
-        Leet_1 solution = new Leet_1();
-        int[] nums = {2, 7, 11, 15};
+        Leet_1 obj = new Leet_1();
+        int[] nums = { 2, 7, 11, 15 };
         int target = 9;
-        int[] result = solution.twoSum(nums, target);
-        System.out.println("Indices: " + result[0] + ", " + result[1]);
+        int[] result = obj.twoSum(nums, target);
+        System.out.println(result[0] + "," + result[1]);
     }
 }
