86375764bec32b1254bd225613120fd938eb1fe3
Kaushik Mandal <koushik369mondal@gmail.com>
2026-03-08 16:23:50 +0530

    add Leet_896 class to implement isMonotonic method for checking monotonic arrays

---
+++ DSA-JAVA/Leetcode/Easy/Leet_896.java
@@ -0,0 +1,31 @@
+package Leetcode.Easy;
+
+public class Leet_896 {
+
+    public boolean isMonotonic(int[] nums) {
+        boolean increasing = true;
+        boolean decreasing = true;
+        
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i - 1]) {
+                decreasing = false;
+            }
+            if (nums[i] < nums[i - 1]) {
+                increasing = false;
+            }
+        }
+        
+        return increasing || decreasing;
+    }
+
+    public static void main(String[] args) {
+        Leet_896 obj = new Leet_896();
+        int[] nums1 = {1, 2, 3, 4};
+        int[] nums2 = {4, 3, 2, 1};
+        int[] nums3 = {1, 3, 2, 4};
+
+        System.out.println(obj.isMonotonic(nums1)); // Output: true
+        System.out.println(obj.isMonotonic(nums2)); // Output: true
+        System.out.println(obj.isMonotonic(nums3)); // Output: false
+    }
+}
---
+++ DSA-JAVA/Leetcode/Hard/Leet_4.java
@@ -0,0 +1,38 @@
+package Leetcode.Hard;
+
+public class Leet_4 {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+        int total = m + n;
+
+        int i = 0, j = 0;
+        int prev = 0, curr = 0;
+
+        for (int k = 0; k <= total / 2; k++) {
+
+            prev = curr;
+
+            if (i < m && (j >= n || nums1[i] < nums2[j])) {
+                curr = nums1[i];
+                i++;
+            } else {
+                curr = nums2[j];
+                j++;
+            }
+        }
+
+        if (total % 2 == 1) {
+            return curr;
+        }
+
+        return (prev + curr) / 2.0;
+    }
+
+    public static void main(String[] args) {
+        Leet_4 obj = new Leet_4();
+        int[] nums1 = {1, 3};
+        int[] nums2 = {2};
+        System.out.println(obj.findMedianSortedArrays(nums1, nums2)); // Output: 2.0
+    }
+}
