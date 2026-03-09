b5c74f8bf02731404177a776bbf3c03bed61633e
Kaushik Mandal <koushik369mondal@gmail.com>
2026-03-09 10:55:00 +0530

    add Leet_137 class to implement singleNumber1 method for finding the single number in an array

---
+++ DSA-JAVA/Leetcode/Easy/Leet_137.java
@@ -0,0 +1,23 @@
+package Leetcode.Easy;
+
+import java.util.Arrays;
+
+public class Leet_137 {
+    public int singleNumber1(int[] nums) {
+        Arrays.sort(nums);
+        int i = 0;
+        while(i < nums.length-1){
+            if(nums[i] == nums[i+1]){
+                i+=3;
+            } else {
+                return nums[i];
+            }
+        }
+        return nums[nums.length-1];
+    }
+    public static void main(String[] args) {
+        Leet_137 obj = new Leet_137();
+        int[] nums = { 2, 2, 3, 2};
+        System.out.println(obj.singleNumber1(nums));
+    }
+}
