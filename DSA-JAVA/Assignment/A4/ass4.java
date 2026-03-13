579b0643dd90cb4bc6351c3da7a50a7d91d34519
Kaushik Mandal <koushik369mondal@gmail.com>
2026-03-14 00:35:06 +0530

    targetSumTriplets method implementation

--- DSA-JAVA/Assignment/A4/ass4.java
+++ DSA-JAVA/Assignment/A4/ass4.java
@@ -10,7 +10,8 @@ public class ass4 {
         // reverseArray();
         // squaresOfSortedArray();
         // maxValueInArray();
-        targetSumPairs();
+        // targetSumPairs();
+        targetSumTriplets();
     }
 
     // Inverse of an array
@@ -32,6 +33,26 @@ public class ass4 {
     }
 
     // Arrays-Target Sum Triplets
+    public static void targetSumTriplets(){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i=0; i<n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int target = sc.nextInt();
+        Arrays.sort(arr); 
+        for (int i=0; i<n; i++) {
+            for (int j=i+1; j<n; j++) {
+                for (int k=j+1; k<n; k++) {
+                    if (arr[i] + arr[j] + arr[k] == target) {
+                        System.out.println(arr[i] + ", " + arr[j] + " and " + arr[k]);
+                    }
+                }
+            }
+        }
+        sc.close();
+    }
 
     // Arrays-Linear Search
     public static void linearSearch(){
