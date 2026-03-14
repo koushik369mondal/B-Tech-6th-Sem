c635ab58d6216ac9a92cbcf9b421bd211638013e
Kaushik Mandal <koushik369mondal@gmail.com>
2026-03-14 12:23:34 +0530

    implement selectionSort method and update main to call it

--- DSA-JAVA/Assignment/A4/ass4.java
+++ DSA-JAVA/Assignment/A4/ass4.java
@@ -13,7 +13,8 @@ public class ass4 {
         // targetSumPairs();
         // targetSumTriplets();
         // sortZeroesAndOnes();
-        bubbleSort();
+        // bubbleSort();
+        selectionSort();
     }
 
     // Inverse of an array
@@ -203,6 +204,29 @@ public class ass4 {
     // Maximum Sum Path in Two Arrays
 
     // Arrays-Selection Sort
+    public static void selectionSort(){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i=0; i<n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int i=0; i<n-1; i++) {
+            int minIndex = i;
+            for (int j=i+1; j<n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+        for (int i=0; i<n; i++) {
+            System.out.println(arr[i] + " ");
+        }
+        sc.close();
+    }
 
     // Arrays-Insertion Sort
 
