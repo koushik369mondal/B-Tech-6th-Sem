891c26630ba897f2817a5f06410f6148f4f189d8
Kaushik Mandal <koushik369mondal@gmail.com>
2026-03-14 23:22:57 +0530

    implement majorityElement method and update main to call it

--- DSA-JAVA/Assignment/A4/ass4.java
+++ DSA-JAVA/Assignment/A4/ass4.java
@@ -16,7 +16,8 @@ public class ass4 {
         // bubbleSort();
         // selectionSort();
         // insertionSort();
-        productOfArrayExceptSelf();
+        // productOfArrayExceptSelf();
+        majorityElement();
     }
 
     // Inverse of an array
@@ -290,6 +291,29 @@ public class ass4 {
     // Rain Water Trapping
 
     // Majority Element
+    public static void majorityElement(){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i=0; i<n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int result = 0;
+        for (int i=0; i<n; i++) {
+            int count = 0;
+            for (int j=0; j<n; j++) {
+                if (arr[i] == arr[j]) {
+                    count++;
+                }
+            }
+            if (count > n/2) {
+                result = arr[i];
+                break;
+            }
+        }
+        System.out.print(result);
+        sc.close();
+    }
 
     // Next Permutation
 }
---
+++ DSA-JAVA/Leetcode/Easy/Leet_169.java
@@ -0,0 +1,27 @@
+package Leetcode.Easy;
+
+public class Leet_169 {
+    public static void main(String[] args) {
+        Leet_169 obj = new Leet_169();
+        int[] nums = {2, 2, 1, 1, 1, 2, 2};
+        System.out.println(obj.majorityElement(nums));
+    }
+
+    public int majorityElement(int[] nums) {
+        // Brute Force
+        int n = nums.length;
+        for (int i=0; i<n; i++) {
+            int count = 0;
+            for (int j=0; j<n; j++) {
+                if (nums[i] == nums[j]) {
+                    count++;
+                }
+            }
+            if (count > n/2) {
+                return nums[i];
+            }
+        }
+        return -1; 
+
+    }
+}
