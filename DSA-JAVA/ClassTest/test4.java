15b90c502c3be2ff800e55b4518e1de715f46089
Kaushik Mandal <koushik369mondal@gmail.com>
2026-03-19 23:02:25 +0530

    Add trappingRainWater method to calculate trapped rainwater

--- DSA-JAVA/ClassTest/test4.java
+++ DSA-JAVA/ClassTest/test4.java
@@ -6,7 +6,8 @@ public class test4 {
     public static void main(String[] args) {
         // rotate();
         // inverseOfArray();
-        maxSum();
+        // maxSum();
+        trappingRainWater();
     }
 
     // rotate array by k times
@@ -83,6 +84,30 @@ public class test4 {
     }
 
     // trapping rain water ***
+    public static void trappingRainWater(){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        int prefix[] = new int[n];
+        int suffix[] = new int[n];
+        prefix[0] = arr[0];
+        for(int i=1; i<n; i++){
+            prefix[i] = Math.max(prefix[i-1], arr[i]);
+        }
+        suffix[n-1] = arr[n-1];
+        for(int i=n-2; i>=0; i--){
+            suffix[i] = Math.max(suffix[i+1], arr[i]);
+        }
+        int water = 0;
+        for(int i=0; i<n; i++){
+            water += Math.min(prefix[i], suffix[i]) - arr[i];
+        }
+        System.out.println(water);
+        sc.close();
+    }
 
     // product of array except self
 
