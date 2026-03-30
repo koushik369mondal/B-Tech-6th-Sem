a68db67052813f5b1dbbfe8168c14efa6d4d5481
Kaushik Mandal <koushik369mondal@gmail.com>
2026-03-30 19:58:20 +0530

    Add EvenOdd method to print even and odd numbers from a 2D array

--- DSA-JAVA/CW/Day24/lec_24.java
+++ DSA-JAVA/CW/Day24/lec_24.java
@@ -48,9 +48,40 @@ public class lec_24 {
         sc.close();
     }
 
+    // print Even and Odd
+    public static void EvenOdd(){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int arr[][] = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if(arr[i][j] % 2 == 0){
+                    System.out.print("Even: " + arr[i][j] + " ");
+                } else {
+                    System.out.print("Odd: " + arr[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+
+    // Linear Search in 2D Array
+
+    // Binary Search in 2D Array
+
+
     public static void main(String[] args) {
         // TwoDArray();
-        findMax();
+        // findMax();
+        EvenOdd();
     }
     
 }
