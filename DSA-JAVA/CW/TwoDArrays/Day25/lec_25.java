7c249d633e691ec534ab8260e2b22c0357affa8e
Kaushik Mandal <koushik369mondal@gmail.com>
2026-03-31 20:24:23 +0530

    Implement spiral matrix method and update main to call it; comment out multiplication

--- DSA-JAVA/CW/TwoDArrays/Day25/lec_25.java
+++ DSA-JAVA/CW/TwoDArrays/Day25/lec_25.java
@@ -24,7 +24,8 @@ public class lec_25 {
 
         // wavePrint(arr);
         // transpose(arr);
-        multiplication(arr);
+        // multiplication(arr);
+        spiralMatrix(arr);
         sc.close();
     }
 
@@ -64,7 +65,40 @@ public class lec_25 {
         }
     }
 
-    // spiral print
+    // spiral matrix
+    public static void spiralMatrix(int arr[][]) {
+        int startRow = 0;
+        int startCol = 0;
+        int endRow = arr.length - 1;
+        int endCol = arr[0].length - 1;
+        System.out.println("Spiral Matrix: ");
+        while (startRow <= endRow && startCol <= endCol) {
+            // top
+            for (int j = startCol; j <= endCol; j++) {
+                System.out.print(arr[startRow][j] + " ");
+            }
+            // right
+            for (int i = startRow + 1; i <= endRow; i++) {
+                System.out.print(arr[i][endCol] + " ");
+            }
+            // bottom
+            if (startRow < endRow) {
+                for (int j = endCol - 1; j >= startCol; j--) {
+                    System.out.print(arr[endRow][j] + " ");
+                }
+            }
+            // left
+            if (startCol < endCol) {
+                for (int i = endRow - 1; i > startRow; i--) {
+                    System.out.print(arr[i][startCol] + " ");
+                }
+            }
+            startRow++;
+            startCol++;
+            endRow--;
+            endCol--;
+        }
+    }
 
     // multiplication 
     public static void multiplication(int arr[][]){
